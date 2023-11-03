3a36ddc884a764c206ed263478743f2a18fb3eee
SuvarneshKM <suvarnesh1729@gmail.com>
2023-11-03 14:53:45 +0530

    fix: deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ekuttan.github.io",
+  site: "https://suvarneshkm.github.io",
   base: "/",
   integrations: [tailwind()],
 });
